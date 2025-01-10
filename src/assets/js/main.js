4f3c3d9241eaacafa0c0b8d19f0db3459ee18b6e
nathanmontt <contato.mont@gmail.com>
2025-01-09 21:03:12 -0300

    Testes
    Algo não está dando certo. Ainda faltam verificações e leitura de documentação

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,55 +1,54 @@
+'use strict'
+
 /* Função para carregar componentes HTML dinamicamente, ou seja, FETCH (buscar, trazer, em português)
  * Dois parâmetros:
  * url: o caminho do arquivo HTML do que a gente quer colocar na main sem ter que escrever todo o código novamente e se repetir, não utilizando o conceito DRY (Don't Repeat Yourself), ex: "components/header.html"
  * containerId: o ID do elemento HTML onde o conteúdo será inserido (ex: "header-container")
 */
 
-document.addEventListener("DOMContentLoaded", function () {
-    function loadComponent(url, containerId) {
-        // Localiza o elemento no DOM pelo ID, ou seja, a partir do nome do ID que colocaremos na função, armazenaremos dentro do container
-        const containerHolder = document.getElementById(containerId);
-        
-        // Faz uma requisição pelo caminho do arquivo para buscar o conteúdo HTML
-        fetch(url)
-        .then(verifyResponse => {
-            if (!verifyResponse.ok) throw new Error(`Erro ao carregar ${url}`);
+function loadComponent(url, containerId) {
+    // Localiza o elemento no DOM pelo ID, ou seja, a partir do nome do ID que colocaremos na função, armazenaremos dentro do container
+    const containerHolder = document.getElementById(containerId);
+    
+    // Faz uma requisição pelo caminho do arquivo para buscar o conteúdo HTML
+    fetch(url)
+    .then(verifyResponse => {
+        if (!verifyResponse.ok) throw new Error(`Erro ao carregar ${url}`);
 
-            // Retorna o conteúdo do arquivo como texto caso a response tenha dado certo
-            return verifyResponse.text();
-        })
-        .then(htmlContent => {
-            // Insere o conteúdo HTML carregado dentro do elemento identificado pelo containerId
-            containerHolder.innerHTML = htmlContent;
-        })
-        .catch(error => {
-            console.error(error);
-        });
-
-        return true;
-    }
-
-    loadComponent("components/header.html", "header-container");
-
-    // function toogleMenuHandler () {
-        
-    // }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const openMenuBtn = document.getElementById("teste");
-    const menuItemsHolder = document.querySelector(".menu-options");
-    const itemLinks = document.querySelectorAll(".options-holder");
-
-    document.addEventListener("DOMContentLoaded", function () {
-        openMenuBtn.addEventListener("click", function () {
-            console.log("teste");
-            // itemLinks.forEach (function (item) {
-            //     if (item.classList.contains("hidden")) {
-            //         item.classList.remove("hidden");
-            //     } else {
-            //         item.classList.add("hidden");
-            //     }
-            // })
-        });
+        // Retorna o conteúdo do arquivo como texto caso a response tenha dado certo
+        return verifyResponse.text();
     })
+    .then(htmlContent => {
+        // Insere o conteúdo HTML carregado dentro do elemento identificado pelo containerId
+        containerHolder.innerHTML = htmlContent;
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+    return true;
+}
+
+loadComponent("components/header.html", "header-container");
+
+/* ----------------------------------------------------------------------------------------------- */
+
+/* MENU* HAMBURGUER */
+const hambMenu = document.getElementById("hamb-menu-btn");
+const menuItemsHolder = document.querySelector(".menu-options");
+const itemLinks = document.querySelectorAll(".options-holder");
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("DOMContentLoaded", function () {
+        hambMenu.addEventListener("click", function () {
+            console.log("teste");
+            itemLinks.forEach (function (item) {
+                if (item.classList.contains("hidden")) {
+                    item.classList.remove("hidden");
+                } else {
+                    item.classList.add("hidden");
+                }
+            });
+        });
+    });
 });
