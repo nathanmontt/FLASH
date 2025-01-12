39b7aae88436d9dd22a990efc98651bc2304cee5
nathanmontt <contato.mont@gmail.com>
2025-01-12 13:04:21 -0300

    Testes (2)
    Usando document.addEventListener('DOMContentLoaded', function () {});

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,19 +1,22 @@
 'use strict';
 
-const hambMenuBtn = document.getElementById('hamb-menu-btn');
-const menuItems = document.querySelectorAll('.options-holder');
 
-// Check if elements exist before adding event listeners
-if (hambMenuBtn && menuItems.length > 0) {
-    hambMenuBtn.addEventListener('click', function () {
-        // Toggle 'hidden' class on each menu item
-        menuItems.forEach(element => {
-            element.classList.toggle('hidden');
+document.addEventListener('DOMContentLoaded', function () {
+    const hambMenuBtn = document.getElementById('hamb-menu-btn');
+    const menuItems = document.querySelectorAll('.options-holder');
+
+    // Check if elements exist before adding event listeners
+    if (hambMenuBtn && menuItems.length > 0) {
+        hambMenuBtn.addEventListener('click', function () {
+            // Toggle 'hidden' class on each menu item
+            menuItems.forEach(element => {
+                element.classList.toggle('hidden');
+            });
+
+            // Toggle 'active' class on the button for animation
+            hambMenuBtn.classList.toggle('active');
         });
-
-        // Toggle 'active' class on the button for animation
-        hambMenuBtn.classList.toggle('active');
-    });
-} else {
-    console.error('Hamburger button or menu items not found in the DOM');
-}
+    } else {
+        console.error('Hamburger button or menu items not found in the DOM');
+    }
+});
