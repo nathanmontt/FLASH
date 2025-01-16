5ba18557725efc45337fe12ded37cffb4e1b09de
nathanmontt <contato.mont@gmail.com>
2025-01-16 09:23:12 -0300

    Lógica
    Mudança da lógica na parte do menu pelo JavaScritp

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,12 +1,20 @@
 const hambMenuBtn = document.getElementById('hamb-menu-btn');
 const menuItems = document.querySelectorAll('.options-holder');
+const hambMenuBtnClore = document.querySelector('.hamb-menu-btn-close');
 
 // Check if elements exist before adding event listeners
 if (hambMenuBtn && menuItems.length > 0) {
     hambMenuBtn.addEventListener('click', function () {
         // Toggle 'hidden' class on each menu item
         menuItems.forEach(element => {
-            element.classList.toggle('hidden');
+            if (element.classList.contains('hidden')) {
+                element.classList.remove('hidden');
+                hambMenuBtn.textContent = 'FECHAR';
+            } else {
+                element.classList.add('hidden');
+                hambMenuBtn.style.display = 'block';
+                hambMenuBtn.textContent = 'MENU';
+            }
         });
 
         // Toggle 'active' class on the button for animation
