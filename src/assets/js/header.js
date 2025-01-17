ecd78175d8c8b989a9a531a9189e32be5d97c9cd
nathanmontt <contato.mont@gmail.com>
2025-01-17 11:30:07 -0300

    Adição
    Finalização do Menu e início do resto do projeto

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,13 +1,13 @@
 const hambMenuBtn = document.getElementById('hamb-menu-btn');
-const menuItemsHolder = document.querySelectorAll('.menu-options-holder');
+const menuItems = document.querySelectorAll('.options-holder');
 // const hambMenuBtnClore = document.querySelector('.hamb-menu-btn-close');
 const overlay = document.querySelector('.overlay');
 
 // Check if elements exist before adding event listeners
-if (hambMenuBtn && menuItemsHolder.length > 0) {
+if (hambMenuBtn && menuItems.length > 0) {
     hambMenuBtn.addEventListener('click', function () {
         // Toggle 'hidden' class on each menu item
-        menuItemsHolder.forEach(element => {
+        menuItems.forEach(element => {
             if (element.classList.contains('hidden')) {
                 element.classList.remove('hidden');
                 hambMenuBtn.textContent = 'FECHAR';
