b44819495125070ae6874e08e96cf7b47b762e46
nathanmontt <contato.mont@gmail.com>
2025-01-17 17:49:27 -0300

    Testes
    Alguns testes. O modo anterior de fazer o site é complicado.
    
    É possível que eu tenha de mudá-lo por completo

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,27 +6,39 @@
  * containerId: o ID do elemento HTML onde o conteúdo será inserido (ex: "header-container")
 */
 
-function loadComponent(url, containerId) {
-    // Localiza o elemento no DOM pelo ID, ou seja, a partir do nome do ID que colocaremos na função, armazenaremos dentro do container
-    const containerHolder = document.getElementById(containerId);
+// function loadComponent(url, containerId) {
+//     // Localiza o elemento no DOM pelo ID, ou seja, a partir do nome do ID que colocaremos na função, armazenaremos dentro do container
+//     const containerHolder = document.getElementById(containerId);
     
-    // Faz uma requisição pelo caminho do arquivo para buscar o conteúdo HTML
-    fetch(url)
-    .then(verifyResponse => {
-        if (!verifyResponse.ok) throw new Error(`Erro ao carregar ${url}`);
+//     // Faz uma requisição pelo caminho do arquivo para buscar o conteúdo HTML
 
-        // Retorna o conteúdo do arquivo como texto caso a response tenha dado certo
-        return verifyResponse.text();
-    })
-    .then(htmlContent => {
-        // Insere o conteúdo HTML carregado dentro do elemento identificado pelo containerId
-        containerHolder.innerHTML = htmlContent;
-    })
-    .catch(error => {
-        console.error(error);
-    });
+//     fetch(url)
+//     .then(verifyResponse => {
+//         if (!verifyResponse.ok) throw new Error(`Erro ao carregar ${url}`);
 
-    return true;
-}
+//         // Retorna o conteúdo do arquivo como texto caso a response tenha dado certo
+//         return verifyResponse.text();
+//     })
+//     .then(htmlContent => {
+//         // Insere o conteúdo HTML carregado dentro do elemento identificado pelo containerId
+//         containerHolder.innerHTML = htmlContent;
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
 
-loadComponent("pages/hero.html", "hero-container");
+//     return true;
+// }
+
+// loadComponent("pages/hero.html", "content-container");
+
+// document.querySelectorAll('[nav-links]').forEach(link => {
+//     const contentContainer = document.getElementById('content-container');
+
+//     link.onclick = function (element) {
+//         element.preventDefault();
+//         fetch(link.getAttribute('nav-links'))
+//             .then(response => response.text())
+//             .then(html => contentContainer.innerHTML = html)
+//     }
+// })
