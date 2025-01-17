d43b483fde678004200770699bad0a66fbaa150a
nathanmontt <contato.mont@gmail.com>
2025-01-17 10:57:07 -0300

    Correção
    Correção na abordagem do Menu

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,24 +1,24 @@
 const hambMenuBtn = document.getElementById('hamb-menu-btn');
-const menuItems = document.querySelectorAll('.options-holder');
-const hambMenuBtnClore = document.querySelector('.hamb-menu-btn-close');
+const menuItemsHolder = document.querySelectorAll('.menu-options-holder');
+// const hambMenuBtnClore = document.querySelector('.hamb-menu-btn-close');
 const overlay = document.querySelector('.overlay');
 
 // Check if elements exist before adding event listeners
-if (hambMenuBtn && menuItems.length > 0) {
+if (hambMenuBtn && menuItemsHolder.length > 0) {
     hambMenuBtn.addEventListener('click', function () {
         // Toggle 'hidden' class on each menu item
-        menuItems.forEach(element => {
+        menuItemsHolder.forEach(element => {
             if (element.classList.contains('hidden')) {
                 element.classList.remove('hidden');
                 hambMenuBtn.textContent = 'FECHAR';
                 
-                overlay.classList.remove('hidden');
+                // overlay.classList.remove('hidden');
             } else {
                 element.classList.add('hidden');
                 hambMenuBtn.style.display = 'block';
                 hambMenuBtn.textContent = 'MENU';
 
-                overlay.classList.add('hidden');
+                // overlay.classList.add('hidden');
             }
         });
     });
