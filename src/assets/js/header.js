3340b707743f424e8acfd7aee5111fffabe6eb64
nathanmontt <contato.mont@gmail.com>
2025-01-20 08:25:51 -0300

    Mudança (2)
    O motivo não é o DOMContentLoaded pelo visto

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,25 +1,27 @@
-const hambMenuBtn = document.getElementById('hamb-menu-btn');
-const menuItems = document.querySelectorAll('.options-holder');
-const menuItemsHolder = document.querySelector('.menu-options-holder');
+document.addEventListener('DOMContentLoaded', function () {
+    const hambMenuBtn = document.getElementById('hamb-menu-btn');
+    const menuItems = document.querySelectorAll('.options-holder');
+    const menuItemsHolder = document.querySelector('.menu-options-holder');
 
-// Check if elements exist before adding event listeners
-if (hambMenuBtn && menuItems.length > 0) {
-    hambMenuBtn.addEventListener('click', function () {
-        console.log("teste 1");
-        // Toggle 'hidden' class on each menu item
-        menuItems.forEach(element => {
-            if (element.classList.contains('hidden')) {
-                console.log("teste 1");
-                element.classList.remove('hidden');
-                hambMenuBtn.textContent = 'FECHAR';
+    // Check if elements exist before adding event listeners
+    if (hambMenuBtn && menuItems.length > 0) {
+        hambMenuBtn.addEventListener('click', function () {
+            console.log("teste 1");
+            // Toggle 'hidden' class on each menu item
+            menuItems.forEach(element => {
+                if (element.classList.contains('hidden')) {
+                    element.classList.remove('hidden');
+                    console.log("teste 1");
+                    hambMenuBtn.textContent = 'FECHAR';
 
-                menuItemsHolder.classList.add('pg-1');
-            } else {
-                element.classList.add('hidden');
-                hambMenuBtn.textContent = 'MENU';
+                    menuItemsHolder.classList.add('pg-1');
+                } else {
+                    element.classList.add('hidden');
+                    hambMenuBtn.textContent = 'MENU';
 
-                menuItemsHolder.classList.remove('pg-1');
-            }
+                    menuItemsHolder.classList.remove('pg-1');
+                }
+            });
         });
-    });
-}
+    }
+});
