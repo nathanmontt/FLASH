6f11664eea07a0134cea0de4aa9ac70ffa98e859
nathanmontt <contato.mont@gmail.com>
2025-01-20 08:24:03 -0300

    Mudança
    Por algum motivo, o menu, visto que ontem funcionava, não está abrindo novamente. Verificar do porque disso

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -5,18 +5,17 @@ const menuItemsHolder = document.querySelector('.menu-options-holder');
 // Check if elements exist before adding event listeners
 if (hambMenuBtn && menuItems.length > 0) {
     hambMenuBtn.addEventListener('click', function () {
+        console.log("teste 1");
         // Toggle 'hidden' class on each menu item
         menuItems.forEach(element => {
             if (element.classList.contains('hidden')) {
+                console.log("teste 1");
                 element.classList.remove('hidden');
                 hambMenuBtn.textContent = 'FECHAR';
 
                 menuItemsHolder.classList.add('pg-1');
-                
-                // overlay.classList.remove('hidden');
             } else {
                 element.classList.add('hidden');
-                // hambMenuBtn.style.display = 'block';
                 hambMenuBtn.textContent = 'MENU';
 
                 menuItemsHolder.classList.remove('pg-1');
